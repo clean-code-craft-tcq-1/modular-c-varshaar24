cea5b304a63f1935dc53e8b34162b76fdfd367d3
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-20 12:27:30 +0530

    Update Display.c

--- Display.c
+++ Display.c
@@ -2,7 +2,7 @@
 #include "Types.h"
 
 void ColorPairToString(const ColorPair_tst* colorPair_st, char* buffer_au8) {
-    sprintf(buffer, "%s %s",
+    sprintf(buffer_au8, "%s %s",
         MajorColorNames_au8[colorPair_st->majorColor_en],
         MinorColorNames_au8[colorPair_st->minorColor_en]);
 }
