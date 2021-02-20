59d867d3962140435f03530f83f931849d326de5
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-20 12:27:02 +0530

    Update Convert.c

--- Convert.c
+++ Convert.c
@@ -1,16 +1,16 @@
 #include "Types.h"
 
-ColorPair_tst GetColorFromPairNumber(int pairNumber) {
+ColorPair_tst GetColorFromPairNumber(int pairNumber_u8) {
     ColorPair_tst colorPair_st;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair_st.majorColor = 
-        (enum MajorColor_ten)(zeroBasedPairNumber / numberOfMinorColors_u8);
-    colorPair_st.minorColor =
-        (enum MinorColor_ten)(zeroBasedPairNumber % numberOfMinorColors_u8);
+    int zeroBasedPairNumber_u8 = pairNumber_u8 - 1;
+    colorPair_st.majorColor_en = 
+        (enum MajorColor_ten)(zeroBasedPairNumber_u8 / numberOfMinorColors_u8);
+    colorPair_st.minorColor_en =
+        (enum MinorColor_ten)(zeroBasedPairNumber_u8 % numberOfMinorColors_u8);
     return colorPair_st;
 }
 
 int GetPairNumberFromColor(const ColorPair_tst* colorPair_st) {
-    return colorPair_st->majorColor * numberOfMinorColors_u8 +
-            colorPair_st->minorColor + 1;
+    return colorPair_st->majorColor_en * numberOfMinorColors_u8 +
+            colorPair_st->minorColor_en + 1;
 }
