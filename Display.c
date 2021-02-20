8196e760349b376d8e3e97703f736f9c3747b33f
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-20 12:20:17 +0530

    Create Display.c

---
+++ Display.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Types.h"
+
+void ColorPairToString(const ColorPair_tst* colorPair_st, char* buffer_au8) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames_au8[colorPair_st->majorColor_en],
+        MinorColorNames_au8[colorPair_st->minorColor_en]);
+}
