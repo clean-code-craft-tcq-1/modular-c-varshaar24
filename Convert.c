96df73790bc88e90270510d951f10b3c0df83119
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-23 19:59:22 +0530

    Update Convert.c

--- Convert.c
+++ Convert.c
@@ -1,17 +1,40 @@
 #include "Types.h"
 
+/******************************************/
+/* Externs */
+/******************************************/
+extern const char* MajorColorNames_au8;
+extern const char* MinorColorNames_au8;
+
+/******************************************/
+/* Globar variables */
+/******************************************/
+int numberOfMajorColors_u8 =
+    sizeof(MajorColorNames_au8) / sizeof(MajorColorNames_au8[0]);
+
+
+int numberOfMinorColors_u8 =
+    sizeof(MinorColorNames_au8) / sizeof(MinorColorNames_au8[0]);
+
+/******************************************/
+/* GetColorFromPairNumber */
+/******************************************/
 ColorPair_tst GetColorFromPairNumber(int pairNumber_u8) {
     ColorPair_tst colorPair_st;
     int zeroBasedPairNumber_u8 = pairNumber_u8 - 1;
-    colorPair_st.majorColor_en = 
+    colorPair_st.majorColor_en =
         (enum MajorColor_ten)(zeroBasedPairNumber_u8 / numberOfMinorColors_u8);
     colorPair_st.minorColor_en =
         (enum MinorColor_ten)(zeroBasedPairNumber_u8 % numberOfMinorColors_u8);
     return colorPair_st;
 }
 
+/******************************************/
+/* GetPairNumberFromColor */
+/******************************************/
 int GetPairNumberFromColor(const ColorPair_tst* colorPair_st) {
     return colorPair_st->majorColor_en * numberOfMinorColors_u8 +
             colorPair_st->minorColor_en + 1;
 }
+
 
