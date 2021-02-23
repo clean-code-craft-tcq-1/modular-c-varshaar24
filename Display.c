7920b3757da1c499938cb01f3852bf42c896a108
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-23 19:00:50 +0530

    Update Display.c

--- Display.c
+++ Display.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include "Types.h"
 
-extern const char* MajorColorNames_au8[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-extern const char* MinorColorNames_au8[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
 void ColorPairToString(const ColorPair_tst* colorPair_st, char* buffer_au8) {
     sprintf(buffer_au8, "%s %s",
         MajorColorNames_au8[colorPair_st->majorColor_en],
