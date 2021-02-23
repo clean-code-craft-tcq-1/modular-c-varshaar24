47d2cfc4e6976874361b1ac025edf961fd91d164
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-23 18:52:26 +0530

    Update Display.c

--- Display.c
+++ Display.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "Types.h"
 
-const char* MajorColorNames_au8[] = {
+extern const char* MajorColorNames_au8[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-const char* MinorColorNames_au8[] = {
+extern const char* MinorColorNames_au8[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
