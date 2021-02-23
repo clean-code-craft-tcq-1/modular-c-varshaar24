2d0f15305863cd291b879cd129f4d57fee69ee42
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-23 20:53:47 +0530

    Update Display.c

--- Display.c
+++ Display.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "Types.h"
 
+extern const int MAX_COLORPAIR_NAME_CHARS;
+
 const char* MajorColorNames_au8[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
@@ -20,4 +22,17 @@ void ColorPairToString(const ColorPair_tst* colorPair_st, char* buffer_au8) {
     sprintf(buffer_au8, "%s %s",
         MajorColorNames_au8[colorPair_st->majorColor_en],
         MinorColorNames_au8[colorPair_st->minorColor_en]);
+}
+
+void DisplayColourPairForm(void)
+{
+    for(int colournum =1;colournum<=25;colournum++)
+    {
+        printf("%d ->",colournum);
+
+        ColorPair_tst EachColurPair_st = GetColorFromPairNumber(colournum);
+        char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+        ColorPairToString(&EachColurPair_st, colorPairNames);
+        printf("%s\n",colorPairNames);
+    }
 }
