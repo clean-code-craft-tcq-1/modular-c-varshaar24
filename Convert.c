ec83fcdf5fc32166c66ed5258942352e3e83049e
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-02-23 19:00:28 +0530

    Update Convert.c

--- Convert.c
+++ Convert.c
@@ -1,12 +1,5 @@
 #include "Types.h"
 
-extern int numberOfMajorColors_u8 =
-    sizeof(MajorColorNames_au8) / sizeof(MajorColorNames_au8[0]);
-
-
-extern int numberOfMinorColors_u8 =
-    sizeof(MinorColorNames_au8) / sizeof(MinorColorNames_au8[0]);
-
 ColorPair_tst GetColorFromPairNumber(int pairNumber_u8) {
     ColorPair_tst colorPair_st;
     int zeroBasedPairNumber_u8 = pairNumber_u8 - 1;
@@ -21,3 +14,4 @@ int GetPairNumberFromColor(const ColorPair_tst* colorPair_st) {
     return colorPair_st->majorColor_en * numberOfMinorColors_u8 +
             colorPair_st->minorColor_en + 1;
 }
+
